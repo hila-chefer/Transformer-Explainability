fix BERT explainability CLS calculation
assign the minimal value to the CLS token instead of 0 to avoid changing the scale in the normalization between 0 and 1.

BERT_explainability/modules/BERT/ExplanationGenerator.py
@@ -55,7 +55,7 @@ class Generator:
             cam = cam.clamp(min=0).mean(dim=0)
             cams.append(cam.unsqueeze(0))
         rollout = compute_rollout_attention(cams, start_layer=start_layer)
-        rollout[:, 0, 0] = 0
+        rollout[:, 0, 0] = rollout[:, 0].min()
         return rollout[:, 0]
 
 